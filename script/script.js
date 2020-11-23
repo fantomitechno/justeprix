454f3a8d6703547d0a9af521dead5788a6f350be
FANTOMITECHNO FANT0MIB0T <fantomitechno@gmail.com>
2020-11-23 19:26:39 +0100

    t

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ function randomNb(max,min) {
 }
 
 let prix = [randomNb(500,200),randomNb(1000,700),randomNb(1000,700),randomNb(700,300),randomNb(700,300),randomNb(200,50),randomNb(700,300),randomNb(500,200),randomNb(500,200),randomNb(500,200)]
+let prixMinMax = ["200 - 500", "700 - 1000", "700 - 1000", "300 - 700", "300 - 700", "50 - 200", "300 - 700", "200 - 500", "200 - 500", "200 - 500"]
 let essais = 40
 let essais_start = essais
 let towin = 0
@@ -20,12 +21,12 @@ while (nb4 === nb1 || nb4 === nb2 || nb4 === nb3) {
     nb4 = randomNb(10,1)
 }
 function next() {
-    if (towin = 1) document.getElementById('reset').innerHTML=`<tbody><tr><td><img src=images/produit${nb2}.jpg></td></tr><tr><td><form name='formulaire2'><input type='text' name='saisie2' size='10'><input type='button' name='bp2' value='CHECK' onclick='tester2()'></form></td></tr></tbody>`
-    if (towin = 2) document.getElementById('reset').innerHTML=`<tbody><tr><td><img src=images/produit${nb3}.jpg></td></tr><tr><td><form name='formulaire2'><input type='text' name='saisie2' size='10'><input type='button' name='bp2' value='CHECK' onclick='tester2()'></form></td></tr></tbody>`
-    if (towin = 3) document.getElementById('reset').innerHTML=`<tbody><tr><td><img src=images/produit${nb4}.jpg></td></tr><tr><td><form name='formulaire2'><input type='text' name='saisie2' size='10'><input type='button' name='bp2' value='CHECK' onclick='tester2()'></form></td></tr></tbody>`
+    if (towin = 1) document.getElementById('reset').innerHTML=`<tbody><tr><td><img src=images/produit${nb2}.jpg></td><td>Between ${prixMinMax[nb2]}</td></tr><tr><td><form name='formulaire2'><input type='text' name='saisie2' size='10'><input type='button' name='bp2' value='CHECK' onclick='tester2()'></form></td><td></td></tr></tbody>`
+    if (towin = 2) document.getElementById('reset').innerHTML=`<tbody><tr><td><img src=images/produit${nb3}.jpg></td><td>Between ${prixMinMax[nb3]}</td></tr><tr><td><form name='formulaire3'><input type='text' name='saisie3' size='10'><input type='button' name='bp3' value='CHECK' onclick='tester3()'></form></td><td></td></tr></tbody>`
+    if (towin = 3) document.getElementById('reset').innerHTML=`<tbody><tr><td><img src=images/produit${nb4}.jpg></td><td>Between ${prixMinMax[nb4]}</td></tr><tr><td><form name='formulaire4'><input type='text' name='saisie4' size='10'><input type='button' name='bp4' value='CHECK' onclick='tester4()'></form></td><td></td></tr></tbody>`
 }
 function start() {
-    document.getElementById('reset').innerHTML=`<tbody><tr><td><img src=images/produit${nb1}.jpg></td><td><img src=images/produit${nb2}.jpg></td><td><img src=images/produit${nb3}.jpg></td><td><img src=images/produit${nb4}.jpg></td></tr><tr><td><form name='formulaire1'><input type='text' name='saisie1' size='10'><input type='button' name='bp1' value='CHECK' onclick='tester1()'></form></td><td><form name='formulaire2'><input type='text' name='saisie2' size='10'><input type='button' name='bp2' value='CHECK' onclick='tester2()'></form></td><td><form name='formulaire3'><input type='text' name='saisie3' size='10'><input type='button' name='bp3' value='CHECK' onclick='tester3()'></form></td><td><form name='formulaire4'><input type='text' name='saisie4' size='10'><input type='button' name='bp4' value='CHECK' onclick='tester4()'></form></td></tr></tbody>`
+    document.getElementById('reset').innerHTML=`<tbody><tr><td><img src=images/produit${nb1}.jpg></td><td>Between ${prixMinMax[nb1]}</td></tr><tr><td><form name='formulaire1'><input type='text' name='saisie1' size='10'><input type='button' name='bp1' value='CHECK' onclick='tester1()'></form></td><td></td></tr></tbody>`
     document.getElementById('resetbp').innerHTML=''
     document.getElementById('gl').innerHTML='Bonne Chance'
     document.getElementById('essaies').innerHTML=`Essaie(s) restant(s) : ${essais} essaies.` 
@@ -38,7 +39,6 @@ function tester1() {
         towin++
         document.formulaire1.saisie1.value = 'Trouvé !'
         document.formulaire1.saisie1.disabled = true
-        next()
         if (towin == 4) {
             alert('VOUS AVEZ GAGNER')
             document.getElementById('gl').innerHTML=`Vous avez gagné en ${essais_start-essais} essaies.`
@@ -54,8 +54,17 @@ function tester1() {
         }
     } else {
         document.formulaire1.bp1.disabled = true
+        document.formulaire2.bp2.disabled = true
+        document.formulaire3.bp3.disabled = true
+        document.formulaire4.bp4.disabled = true
         document.formulaire1.saisie1.disabled = true
+        document.formulaire2.saisie2.disabled = true
+        document.formulaire3.saisie3.disabled = true
+        document.formulaire4.saisie4.disabled = true
         document.formulaire1.saisie1.value = prix[nb1-1]
+        document.formulaire2.saisie2.value = prix[nb2-1]
+        document.formulaire3.saisie3.value = prix[nb3-1]
+        document.formulaire4.saisie4.value = prix[nb4-1]
         alert('Vous avez perdu !')
         document.getElementById('gl').innerHTML=`Vous avez perdu...`
         document.getElementById('resetbp').innerHTML='<br><INPUT TYPE="button" VALUE="Recommencer" ONCLICK="reset()"></INPUT>'
@@ -70,7 +79,6 @@ function tester2() {
         towin++
         document.formulaire2.saisie2.value = 'Trouvé !'
         document.formulaire2.saisie2.disabled = true
-        next()
         if (towin == 4) {
             alert('VOUS AVEZ GAGNER')
             document.getElementById('gl').innerHTML=`Vous avez gagné en ${essais_start-essais}`
@@ -85,9 +93,18 @@ function tester2() {
             document.formulaire2.saisie2.value = ''
         }
     } else {
+        document.formulaire1.bp1.disabled = true
         document.formulaire2.bp2.disabled = true
+        document.formulaire3.bp3.disabled = true
+        document.formulaire4.bp4.disabled = true
+        document.formulaire1.saisie1.disabled = true
         document.formulaire2.saisie2.disabled = true
+        document.formulaire3.saisie3.disabled = true
+        document.formulaire4.saisie4.disabled = true
+        document.formulaire1.saisie1.value = prix[nb1-1]
         document.formulaire2.saisie2.value = prix[nb2-1]
+        document.formulaire3.saisie3.value = prix[nb3-1]
+        document.formulaire4.saisie4.value = prix[nb4-1]
         alert('Vous avez perdu !')
         document.getElementById('gl').innerHTML=`Vous avez perdu...`
         document.getElementById('resetbp').innerHTML='<br><INPUT TYPE="button" VALUE="Recommencer" ONCLICK="reset()"></INPUT>'
@@ -102,7 +119,6 @@ function tester3() {
         towin++
         document.formulaire3.saisie3.value = 'Trouvé !'
         document.formulaire3.saisie3.disabled = true
-        next()
         if (towin == 4) {
             alert('VOUS AVEZ GAGNER')
             document.getElementById('gl').innerHTML=`Vous avez gagné en ${essais_start-essais}`
@@ -117,9 +133,18 @@ function tester3() {
             document.formulaire3.saisie2.value = ''
         }
     } else {
+        document.formulaire1.bp1.disabled = true
+        document.formulaire2.bp2.disabled = true
         document.formulaire3.bp3.disabled = true
+        document.formulaire4.bp4.disabled = true
+        document.formulaire1.saisie1.disabled = true
+        document.formulaire2.saisie2.disabled = true
         document.formulaire3.saisie3.disabled = true
+        document.formulaire4.saisie4.disabled = true
+        document.formulaire1.saisie1.value = prix[nb1-1]
+        document.formulaire2.saisie2.value = prix[nb2-1]
         document.formulaire3.saisie3.value = prix[nb3-1]
+        document.formulaire4.saisie4.value = prix[nb4-1]
         alert('Vous avez perdu !')
         document.getElementById('gl').innerHTML=`Vous avez perdu...`
         document.getElementById('resetbp').innerHTML='<br><INPUT TYPE="button" VALUE="Recommencer" ONCLICK="reset()"></INPUT>'
@@ -134,7 +159,6 @@ function tester4() {
         towin++
         document.formulaire4.saisie4.value = 'Trouvé !'
         document.formulaire4.saisie4.disabled = true
-        next()
         if (towin == 4) {
             alert('VOUS AVEZ GAGNER')
             document.getElementById('gl').innerHTML=`Vous avez gagné en ${essais_start-essais}`
@@ -149,8 +173,17 @@ function tester4() {
             document.formulaire4.saisie4.value = ''
         }
     } else {
+        document.formulaire1.bp1.disabled = true
+        document.formulaire2.bp2.disabled = true
+        document.formulaire3.bp3.disabled = true
         document.formulaire4.bp4.disabled = true
+        document.formulaire1.saisie1.disabled = true
+        document.formulaire2.saisie2.disabled = true
+        document.formulaire3.saisie3.disabled = true
         document.formulaire4.saisie4.disabled = true
+        document.formulaire1.saisie1.value = prix[nb1-1]
+        document.formulaire2.saisie2.value = prix[nb2-1]
+        document.formulaire3.saisie3.value = prix[nb3-1]
         document.formulaire4.saisie4.value = prix[nb4-1]
         alert('Vous avez perdu !')
         document.getElementById('gl').innerHTML=`Vous avez perdu...`
